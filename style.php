bfa3c1525c52c9ac921c6048ef4ae6218d8590c4
Jan Krepelka <jan.krepelka@gmail.com>
2014-08-22 19:55:13 +0200

    less

--- style.php
+++ style.php
@@ -7,7 +7,19 @@ else {
 	ob_start();
 }
 
-include 'application/view/css/main.css';
+// less
+include 'lib/lessphp.php';
+$less = new lessc;
+try {
+	echo $less->compileFile("application/view/css/main.less");
+}
+catch (exception $e) {
+	die("fatal error: " . $e->getMessage());
+}
+
+// css
+//include 'application/view/css/main.css';
+
 $output = ob_get_contents();
 $output = str_replace("\n", '', $output);
 $output = str_replace("	", '', $output);
@@ -16,7 +28,7 @@ $output = str_replace(", ", ',', $output);
 $output = str_replace(" {", '{', $output);
 ob_end_clean ();
 
-header('Content-type: text/css; charset=utf-8');
+header("Content-type: text/css; charset=utf-8");
 echo $output;
 
 ?>
