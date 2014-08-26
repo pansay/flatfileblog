ba319cf9100ad55ad0ba162257087ee121cc78f5
Jan Krepelka <jan.krepelka@gmail.com>
2014-08-26 23:46:29 +0200

    gzip bug

--- system/engine/renderer.php
+++ system/engine/renderer.php
@@ -2,33 +2,22 @@
 class Renderer {
 
     public static function output ($template, $data) {
-
         if (file_exists($template)) {
             extract ($data);
-
-            if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
-                ob_start("ob_gzhandler");
-            }
-            else {
-                ob_start();
-            }
-                    include $template;
-                    $output = ob_get_contents();
-                    $output = preg_replace('~>\s*\n\s*<~', '><', $output);
-                    $output = str_replace("\n", '', $output);
-
+            ini_set('zlib.output_compression','On');
+            ob_start();
+                include $template;
+                $output = ob_get_contents();
+                $output = preg_replace('~>\s*\n\s*<~', '><', $output);
+                $output = str_replace("\n", '', $output);
             ob_end_clean ();
-
             header('X-UA-Compatible: IE=Edge');
             header('Content-Type: text/html; charset=utf-8');
-
             echo $output;
-
         }
         else {
             die('Error: Could not load template '.$template.'!');
         }
-
     }
 
     public static function rss ($template, $data) {
