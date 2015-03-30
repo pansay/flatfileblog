80b254e593b6d945f7ae8bcb4bb1cbbc1099e2d2
Jan Krepelka <jan.krepelka@gmail.com>
2015-03-30 22:34:22 +0200

    cosmetics

--- system/engine/renderer.php
+++ system/engine/renderer.php
@@ -1,16 +1,17 @@
 <?php
+
 class Renderer {
 
     public static function output ($template, $data) {
         if (file_exists($template)) {
-            extract ($data);
+            extract($data);
             ini_set('zlib.output_compression','On');
             ob_start();
                 include $template;
                 $output = ob_get_contents();
                 $output = preg_replace('~>\s*\n\s*<~', '><', $output);
                 $output = str_replace("\n", '', $output);
-            ob_end_clean ();
+            ob_end_clean();
             header('X-UA-Compatible: IE=Edge');
             header('Content-Type: text/html; charset=utf-8');
             echo $output;
